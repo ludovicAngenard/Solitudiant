521b1e770b50dea2470c878de5333da041e42698
ludovic <ludovic.angenard@orange.fr>
2022-05-06 17:04:02 +0200

    try to add first page

--- templates/solitudiant/index.php
+++ templates/solitudiant/index.php
@@ -4,6 +4,7 @@
 
     <head>
         <jdoc:include type="head" />
+        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/media/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
@@ -11,31 +12,96 @@
     <body>
         <jdoc:include type="modules" name="top" />
         <jdoc:include type="component" />
-        <img  src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/bannière.png" alt="Solitudiant est un challenge étudiant au profit du développement des EA-ESAT et de l'emploi des personnes en situation de handicap." class="customImage" />
-        <div class='button-banner'>
-            <div class="iam">
-                <span>Je suis Etudiant</span>
-            </div>
-            <span>Solitudiant</span>
-            <button class="button" type="button"> S'inscrire sur un projet</button>
-            <button class="button" type="button">Liste des projets</button>
-            <button class="button" type="button">Projets en cours</button>
-            <span>
-                Solistages
-            </span>
-            <button class="button" type="button"> Offre de stages</button>
-            <button class="button" type="button"> Postuler à un stage</button>
+        <div class="mx-auto container">
+            <img  src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/bannière.png" alt="Solitudiant est un challenge étudiant au profit du développement des EA-ESAT et de l'emploi des personnes en situation de handicap." class="customImage" />
+            <div class='d-flex flex-row justify-content-center align-items-start'>
+                <div class='button-banner text-center py-4 rounded mt-5 me-4'>
 
-            <div class='iam'>
-                <span>Je suis une EA/ESAT</span>
+                    <div  class="mt-4 text-white text-start border-bottom mx-auto" style="width:85%;"><h5>Je suis Etudiant</h5></div>
+                    <div  style="width:70%;" class="mx-auto">
+                        <div  class="mt-4 text-white d-flex flex-row justify-content-start">
+                            <img  src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/student.png" alt="Solitudiant" class="me-3"/>
+                            Solitudiant
+                        </div>
+                        <button class="btn btn-primary rounded mt-2" type="button"> S'inscrire sur un projet</button>
+                        <button class="btn btn-primary rounded mt-2" type="button">Liste des projets</button>
+                        <button class="btn btn-primary rounded mt-2" type="button">Projets en cours</button>
+                        <div  class="mt-4 text-white d-flex flex-row justify-content-start">
+                            <img  src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/suitcase.png" alt="Solistage" class="me-3"/>
+                            Solistages
+                        </div>
+                        <button class="btn btn-primary rounded mt-2" type="button"> Offre de stages</button>
+                        <button class="btn btn-primary rounded mt-2" type="button"> Postuler à un stage</button>
+                    </div>
+
+                    <div  class="mt-4 text-white text-start mx-auto border-bottom " style="width:85%;"><h5>Je suis une EA/ESAT</h5></div>
+                    <div  style="width:70%;" class="mx-auto">
+                        <button class="btn btn-primary rounded mt-2" type="button" >Déposer un projet Solitudiant</button>
+                        <button class="btn btn-primary rounded mt-2" type="button">déposer une offre Solistages</button>
+                    </div>
+                    <div  class="mt-4 text-white text-start mx-auto border-bottom"style="width:85%;"><h5>Devenir bénévole</h5></div>
+                    <div  style="width:70%;" class="mx-auto">
+                        <button class="btn btn-primary rounded mt-2" type="button">Devenir ambassadeur</button>
+                        <button class="btn btn-primary rounded mt-2" type="button">Devenir mentor</button>
+                    </div>
+                </div>
+                <div class='mt-5'>
+                    <img  style="max-width:100%;object-fit: contain;" src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/solitudiantBannièreContenu.png" alt="Solitudiant Challenge étudiant handi-solidaire"/>
+                    <p class='text-center small'>Sous le parrainage de M. Didier Rambeaux, président d'Andicat</p>
+                    <div class="key-number mx-auto text-center py-4">
+                        <h4 class="w-75 text-center mx-auto fw-bold mb-4"  style="font-family:Aleo;color:#184080;">Challenge étudiant pour l'emploi des personnes en situation de handicap</h4>
+                        <div class="w-75 mx-auto d-flex flex-row justify-content-between">
+                            <span  style="color:#184080;"><h4 class="fw-bold">26</h4> <p>EA/ESAT</p></span>
+                            <span  style="color:#184080;" ><h5>Chiffre clés de la 7e édition 2021/2022</h5></span>
+                            <span  style="color:#184080;"><h4 class="fw-bold">20</h4> <p>Ecoles Universités</p></span>
+                        </div>
+                        <span  style="color:#184080;"><h4 class="fw-bold">29</h4> <p>groupes d'étudiants</p></span>
+                    </div>
+                    <h2 style="color:#184080;font-family:Aleo;font-size:30pt;" class="text-center my-5 fw-bold">SoliTudiant, qu'est-ce que c'est ?</h2>
+                </div>
             </div>
-            <button class="button" type="button">Déposer un projet Solitudiant</button>
-            <button class="button" type="button">déposer une offre Solistages</button>
-            <span>Devenir bénévole</span>
-            <button class="button" type="button">Devenir ambassadeur</button>
-            <button class="button" type="button">Devenir mentor</button>
+            <div class='w-75 float-end'>
+                <p style="font-family: Open Sans;" class='mx-auto'>
+                    Le challenge SoliTudiant est un challenge national au profit du secteur adapté / protégé, qui s’inscrit pleinement dans l’économie sociale et solidaire.
+                    </br>
+                    </br>
+                    Il permet à des structures employant des personnes en situation de handicap - comme les Entreprises Adaptées (EA) ou les Etablissements et Services d’Aide par le Travail (ESAT) - et à des groupes d’étudiants de réaliser ensemble une étude sur un sujet défini, avec pour objectif de développer l’emploi des personnes en situation de handicap qui travaillent au sein de ces établissements.
+                    </br>
+                    </br>
+                    SoliTudiant c’est également l’occasion idéale pour sen­sibiliser les jeunes à la considération
+                    du handicap en entreprise et solliciter différents domaines de compé­tences
+                    (marketing, communication, commerce, ingénierie etc.) au service d’un projet enrichissant.
+                    </br>
+                    </br>
+                    </br>
+                    L'étude porte sur l’une des 3 thématiques suivantes :
+                    </br>
+                    </br>
+                </p>
+                <div class='d-flex flex-row'>
+                    <div>
+                        <ul style="font-family: Open Sans;color:#4173C4;" >
+                            <li>Promouvoir les produits fabriqués.</li>
+                            <li>Développer de nouvelles filières d'activités pour les travailleurs en</li>
+                            <li>situation de handicap.</li>
+                            <li>Résoudre une problématique interne.</li>
+                        </ul>
+                        <p  style="font-family: Open Sans;" >
+                            </br>
+                            </br>
+                            Les 3 structures lauréates reçoivent jusqu’à 30 000 € de primes* afin de mettre
+                            en œuvre les travaux menés par les étudiants.
+                            </br>
+                            </br>
+                            <span class="small text-body">*Sous réserve de financement du projet par les mécènes d’HTS.</span>
+                        </p>
+                    </div>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/oWhCx3Kox9M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+            </div>
+            <div class="clearfix"></div>
         </div>
-
 
 
         <jdoc:include type="modules" name="bottom" />
